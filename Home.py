12e4e3037124c7b298cee55915e014a6f149ca1c
9Cala4 <116003589+9Cala4@users.noreply.github.com>
2022-11-29 16:14:32 +0100

    cala

--- Home.py
+++ Home.py
@@ -129,6 +129,6 @@ class Home(tk.Frame):
 
     def ImageHH(bildFrame):
         path = 'Bilder/HH.png'
-        img = Image.open(path).resize((1100, 440))  # ((300, 150))
+        img = Image.open(path).resize((1100, 440))
         bildFrame.image = ImageTk.PhotoImage(img)
         tk.Label(bildFrame, image=bildFrame.image).place(x=0, y=0, relwidth=1, relheight=1)
