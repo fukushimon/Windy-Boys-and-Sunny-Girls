45e2dd8b66f48fd3bbef7f0f4e14742e228ba80b
9Cala4 <116003589+9Cala4@users.noreply.github.com>
2022-11-30 19:06:59 +0100

    Cala

--- Home.py
+++ Home.py
@@ -112,7 +112,6 @@ class Home(tk.Frame):
         toolbar = NavigationToolbar2Tk(canvas, bottom_frame)
         toolbar.update()
         canvas._tkcanvas.pack()#grid(row=1, column=0)
-        #hola
 
 
     @classmethod
