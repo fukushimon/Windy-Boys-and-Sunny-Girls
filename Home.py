b2d7083ef2e02d217f2cdb11168e70d23fe22892
9Cala4 <116003589+9Cala4@users.noreply.github.com>
2022-11-30 18:53:55 +0100

    Cala

--- Home.py
+++ Home.py
@@ -111,7 +111,7 @@ class Home(tk.Frame):
         canvas.get_tk_widget().grid(row=0, column=0)
         toolbar = NavigationToolbar2Tk(canvas, bottom_frame)
         toolbar.update()
-        canvas.tkcanvas.pack()#grid(row=1, column=0)
+        canvas._tkcanvas.pack()#grid(row=1, column=0)
 
     @classmethod
     def ImageSH(cls, bildFrame):
