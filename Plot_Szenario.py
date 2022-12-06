f62cf10e8cf8cb04f4c6e4ad3c2a1ee4d07a2004
fukushimon <shimon-kun@gmx.de>
2022-12-06 04:58:35 +0100

    Tired

--- Plot_Szenario.py
+++ Plot_Szenario.py
@@ -24,6 +24,24 @@ solar_potenzialflaechen = {
     'Standorte': ['Schleswig', 'SPO', 'Leck']
     }
 
+solar_5mrd = {
+    'Anlagen': ['SunPower', 'SunPower', 'SunPower'],
+    'Flaeche': [2.464, 3.79, 0.948],
+    'Standorte': ['Schleswig', 'SPO', 'Leck']
+    }
+
+wind_null = {
+    'Anlagen': ['Enercon'],
+    'Anzahl': [0],
+    'Standorte': ['SPO']
+    }
+
+solar_null = {
+    'Anlagen': ['SunPower'],
+    'Flaeche': [0],
+    'Standorte': ['Schleswig']
+    }
+
 # Szenario 'MAX': Wind- und Solar-Potenzialflächen werden vollständig bebaut (inkl.Repowering)
 scene_max = Szenario('Potenzialflaechen', 2030, 1, 
                       wind_potenzialflaechen['Anlagen'] + wind_repowering['Anlagen'], 
@@ -54,8 +72,63 @@ print(scene_max_mix.calc_max_dunkelflaute_ee('Both'))
 
 # scene_ausweis_mix = scene_ausweis.calc_strommix()
 # scene_ausweis_mix.plot_bilanz_ee('Both')
+# scene_ausweis_mix.plot_strommix_ee('Both')
 # print("Szenario Ausweisflächen:")
 # print(scene_ausweis_mix.calc_pct_positive_bilanz_ee('Both'))
 # print(scene_ausweis_mix.calc_dunkelflaute_ee('Both'))
 # print(scene_ausweis_mix.calc_max_dunkelflaute_ee('Both'))
+
+# Nur Wind
+# scene_wind = Szenario('Potenzialflaechen', 2030, 1, 
+#                       wind_potenzialflaechen['Anlagen'] + wind_repowering['Anlagen'], 
+#                       wind_potenzialflaechen['Anzahl'] + wind_repowering['Anzahl'], 
+#                       wind_potenzialflaechen['Standorte'] + wind_repowering['Standorte'], 
+#                       solar_null['Anlagen'], 
+#                       solar_null['Flaeche'], 
+#                       solar_null['Standorte']
+#                       )
+
+# scene_wind_mix = scene_wind.calc_strommix()
+# scene_wind_mix.plot_bilanz_ee('Both')
+# scene_wind_mix.plot_strommix_ee('Both')
+# print("Szenario Wind:")
+# print(scene_wind_mix.calc_pct_positive_bilanz_ee('Both'))
+# print(scene_wind_mix.calc_dunkelflaute_ee('Both'))
+# print(scene_wind_mix.calc_max_dunkelflaute_ee('Both'))
+
+# Nur PV
+# scene_pv = Szenario('Potenzialflaechen', 2030, 1, 
+#                       wind_null['Anlagen'], 
+#                       wind_null['Anzahl'], 
+#                       wind_null['Standorte'], 
+#                       solar_potenzialflaechen['Anlagen'], 
+#                       solar_potenzialflaechen['Flaeche'], 
+#                       solar_potenzialflaechen['Standorte']
+#                       )
+
+# scene_pv_mix = scene_pv.calc_strommix()
+# scene_pv_mix.plot_bilanz_ee('Both')
+# scene_pv_mix.plot_strommix_ee('Both')
+# print("Szenario Solar:")
+# print(scene_pv_mix.calc_pct_positive_bilanz_ee('Both'))
+# print(scene_pv_mix.calc_dunkelflaute_ee('Both'))
+# print(scene_pv_mix.calc_max_dunkelflaute_ee('Both'))
+
+# Szenario 5Mrd
+# scene_5mrd = Szenario('Potenzialflaechen', 2030, 1, 
+#                       wind_ausweisflaechen['Anlagen'] + wind_repowering['Anlagen'], 
+#                       wind_ausweisflaechen['Anzahl'] + wind_repowering['Anzahl'], 
+#                       wind_ausweisflaechen['Standorte'] + wind_repowering['Standorte'], 
+#                       solar_5mrd['Anlagen'], 
+#                       solar_5mrd['Flaeche'], 
+#                       solar_5mrd['Standorte']
+#                       )
+
+# scene_5mrd_mix = scene_5mrd.calc_strommix()
+# scene_5mrd_mix.plot_bilanz_ee('Both')
+# scene_5mrd_mix.plot_strommix_ee('Both')
+# print("Szenario 5 Mrd:")
+# print(scene_5mrd_mix.calc_pct_positive_bilanz_ee('Both'))
+# print(scene_5mrd_mix.calc_dunkelflaute_ee('Both'))
+# print(scene_5mrd_mix.calc_max_dunkelflaute_ee('Both'))
 
--- Szenario.py
+++ Szenario.py
@@ -53,8 +53,8 @@ class Szenario:
     
     def calc_strommix(self):
         new_strommix = Strommix(self.last_szenario, self.year)
-        v_wind = Wind('mean')
-        rad_pv = Globalstrahlung(self.year, 'mean')
+        v_wind = Wind('max')
+        rad_pv = Globalstrahlung(self.year, 'max')
         
         # Wind
         wind = pd.DataFrame({
