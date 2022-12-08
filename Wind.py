d44f66064cc64f2be0803fa6bd2b10c729089803
9Cala4 <116003589+9Cala4@users.noreply.github.com>
2022-12-08 14:02:07 +0100

    Cala

--- Wind.py
+++ Wind.py
@@ -7,6 +7,7 @@ import Home
 import Solar
 from konstante import style
 
+
 class Wind(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -85,43 +86,45 @@ class Wind(tk.Frame):
         Hersteller = tk.Label(produktFrame, text='Hersteller', **style.STYLE,
                               activebackground=style.BACKGROUND, activeforeground=style.TEXT)
         Hersteller.grid(row=0, column=0, padx=5, pady=3)
-        windFrame.cbx_Hersteller = ttk.Combobox(produktFrame, width=50)
+        self.cbx_Hersteller = ttk.Combobox(produktFrame, width=50)
         Hersteller = ('', 'Enercon', 'Vestas', 'Siemens-Gamesa', 'Nordex')
-        windFrame.cbx_Hersteller['values'] = Hersteller
-        windFrame.cbx_Hersteller.current(0)
-        windFrame.cbx_Hersteller.grid(row=0, column=1)
+        self.cbx_Hersteller['values'] = Hersteller
+        self.cbx_Hersteller.current(0)
+        self.cbx_Hersteller.grid(row=0, column=1)
 
         Standort = tk.Label(produktFrame, text='Standort', **style.STYLE,
                              activebackground=style.BACKGROUND, activeforeground=style.TEXT)
         Standort.grid(row=1, column=0, padx=5, pady=3)
-        windFrame.cbx_Standort = ttk.Combobox(produktFrame, width=50)
+        self.cbx_Standort = ttk.Combobox(produktFrame, width=50)
         Standort = ('', 'Schleswig-Holstein A', 'Schleswig-Holstein B', 'Schleswig-Holstein C',
                     'Schleswig-Holstein D', 'Schleswig-Holstein E', 'Schleswig-Holstein F', 'Hamburg')
-        windFrame.cbx_Standort['values'] = Standort
-        windFrame.cbx_Standort.current(0)
-        windFrame.cbx_Standort.grid(row=1, column=1)
+        self.cbx_Standort['values'] = Standort
+        self.cbx_Standort.current(0)
+        self.cbx_Standort.grid(row=1, column=1)
 
         Modellname = tk.Label(produktFrame, text='Modellname', **style.STYLE,
                               activebackground=style.BACKGROUND, activeforeground=style.TEXT)
         Modellname.grid(row=0, column=2, padx=5, pady=3)
-        windFrame.cbx_Modellname = ttk.Combobox(produktFrame, width=50)
+        self.cbx_Modellname = ttk.Combobox(produktFrame, width=50)
         Modellname = ('', 'AAA', 'BBB', 'CCC', 'DDD', 'EEE',)
-        windFrame.cbx_Modellname['values'] = Modellname
-        windFrame.cbx_Modellname.current(0)
-        windFrame.cbx_Modellname.grid(row=0, column=3)
+        self.cbx_Modellname['values'] = Modellname
+        self.cbx_Modellname.current(0)
+        self.cbx_Modellname.grid(row=0, column=3)
 
         label1 = tk.Label(produktFrame, text='Wetter daten werden automatisch beruchsichtig', **style.STYLE,
                           activebackground=style.BACKGROUND, activeforeground=style.TEXT)
         label1.grid(row=1, column=3, padx=5, pady=3)
 
-        Anzahl = tk.Label(produktFrame, text='Anzahl', **style.STYLE,
+        anzahl = tk.Label(produktFrame, text="Anzahl", **style.STYLE,
                           activebackground=style.BACKGROUND, activeforeground=style.TEXT)
-        Anzahl.grid(row=0, column=4, padx=5, pady=3)
-        windFrame.txt_Anzahl = tk.Entry(produktFrame, width=10)
-        windFrame.txt_Anzahl.grid(row=0, column=5)
+        anzahl.grid(row=0, column=4, padx=5, pady=3)
+        self.anzahl_spinbox = tk.Spinbox(produktFrame, width=5, from_=0, to=100)#, from_=0
+        #self.anzahl_spinbox.config(values=0)
+        self.anzahl_spinbox.grid(row=0, column=5)
+
 
         button1 = tk.Button(produktFrame, text='Löschen', **style.STYLE, activebackground=style.BACKGROUND,
-                            activeforeground=style.TEXT)
+                            activeforeground=style.TEXT, command=self.loeschen)
         button1.grid(row=1, column=5, padx=5, pady=3)
 
 
@@ -146,3 +149,10 @@ class Wind(tk.Frame):
     def go_home(self):
         tk.Button(self, text='Home', **style.STYLE, activebackground=style.BACKGROUND, activeforeground=style.TEXT,
                   command=lambda: self.controller.show_frame(Home.Home)).pack(side='bottom', fill=tk.X, padx=10, pady=8)
+
+    def loeschen(self):
+        self.cbx_Hersteller.current(0)
+        self.cbx_Standort.current(0)
+        self.cbx_Modellname.current(0)
+        self.anzahl_spinbox.config(values=0)
+        #anzahl_spinbox = tk.Spinbox(produktFrame, width=5, from_=0, to=100)
