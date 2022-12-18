e3c72c7ca3ddbc41a9349456154973e4225b7bce
9Cala4 <116003589+9Cala4@users.noreply.github.com>
2022-12-18 13:03:39 +0100

    Cala

--- Wind.py
+++ Wind.py
@@ -15,7 +15,7 @@ class Wind(tk.Frame):
 
     def __init__(self, parent, controller):
         super().__init__(parent)
-        self.config(bg='white')  # bg=style.BACKGROUND)
+        self.config(bg=style.BACKGROUND)
         self.controller = controller
         self.button_menu()
         label1 = tk.Label(self, text='Scenario Wind', **style.FONTTITEL,
@@ -31,13 +31,16 @@ class Wind(tk.Frame):
         buttonFrame.pack(side=tk.TOP, fill=tk.X, padx=10, pady=8)
 
         button1 = tk.Button(buttonFrame, text='Wind', **style.STYLE,
-                            activebackground=style.BACKGROUND, activeforeground=style.TEXT, command=lambda: self.controller.show_frame(Wind))
+                            activebackground=style.BACKGROUND, activeforeground=style.TEXT,
+                            command=lambda: self.controller.show_frame(Wind))
         button1.grid(row=0, column=0, padx=5, pady=3)
         button2 = tk.Button(buttonFrame, text='Solar', **style.STYLE,
-                            activebackground=style.BACKGROUND, activeforeground=style.TEXT, command=lambda: self.controller.show_frame(Solar.Solar))
+                            activebackground=style.BACKGROUND, activeforeground=style.TEXT,
+                            command=lambda: self.controller.show_frame(Solar.Solar))
         button2.grid(row=0, column=1, padx=5, pady=3)
         button3 = tk.Button(buttonFrame, text='Speicher', **style.STYLE,
-                            activebackground=style.BACKGROUND, activeforeground=style.TEXT, command=lambda: self.controller.show_frame(Speicher.Speicher))
+                            activebackground=style.BACKGROUND, activeforeground=style.TEXT,
+                            command=lambda: self.controller.show_frame(Speicher.Speicher))
         button3.grid(row=0, column=2, padx=5, pady=3)
 
 
@@ -45,7 +48,7 @@ class Wind(tk.Frame):
     def wind_frame(self):
 
         self.windFrame = tk.Frame(self)
-        self.windFrame.config(bg='yellow')#background=style.BACKGROUND)
+        self.windFrame.config(background=style.BACKGROUND)
         self.windFrame.pack(side=tk.TOP, fill=tk.BOTH, expand=True, padx=10, pady=8)
 
         # Scrollbar hinzufügen
@@ -74,7 +77,7 @@ class Wind(tk.Frame):
         add_button = tk.Button(self.scrollable_frame, text='Neue Anlage +', **style.STYLE,
                                activebackground=style.BACKGROUND, activeforeground=style.TEXT,
                                command=lambda: self.add_produktFrame())
-        add_button.pack(side=tk.TOP, fill=tk.BOTH, padx=10, pady=8)
+        add_button.pack(side=tk.BOTTOM, fill=tk.BOTH, padx=10, pady=8)
 
 #Beschreibung Szenario
         datenFrame = tk.Frame(self.scrollable_frame)
@@ -132,7 +135,7 @@ class Wind(tk.Frame):
                               activebackground=style.BACKGROUND, activeforeground=style.TEXT)
         label_Szenarioname.grid(row=0, column=2, padx=5, pady=3)
         berechnen = tk.Button(leistungFrame, text='Berechnen', **style.STYLE, activebackground=style.BACKGROUND,
-                              activeforeground=style.TEXT, command=lambda: Wind.berechnen(self))
+                              activeforeground=style.TEXT, command=lambda: Wind.faktoren_berücksichtigen(self))
         berechnen.grid(row=0, column=5, padx=5, pady=3)
         Speichern = tk.Button(leistungFrame, text='Speichern', **style.STYLE, activebackground=style.BACKGROUND,
                             activeforeground=style.TEXT)
@@ -149,10 +152,95 @@ class Wind(tk.Frame):
         self.anzahl_spinbox.config(textvariable=var)
 
     def berechnen(self):
-        antwort = messagebox.askyesnocancel('Berechnung', 'asdfghjklöpoiuztrewqyxcvbnm')
-        if antwort == True:
+        confirm = messagebox.askyesnocancel('Berechnung', 'asdfghjklöpoiuztrewqyxcvbnm')
+        if confirm:
             self.controller.show_frame(Energiebilanz.Energiebilanz)
+            self.newWindow.destroy()
+        elif confirm is None:
+            self.newWindow.destroy()
+            self.faktoren_berücksichtigen()
+        else:
+            self.newWindow.destroy()
 
+
+
+    ########################
+
+    def faktoren_berücksichtigen(self):
+        self.newWindow = tk.Toplevel(self)
+        self.newWindow.title("Berechnen")
+        self.newWindow.geometry("400x500")
+        self.newWindow.config(bg=style.BACKGROUND)
+        tk.Label(self.newWindow, text="Faktoren zu berücksichtigen ", **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=0, columnspan=2, sticky=W, padx=5, pady=3)
+
+        tk.Label(self.newWindow, text="Wetterdaten: ", **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=1, columnspan=1, sticky=W, padx=5, pady=3)
+        option1 = IntVar()
+        option2 = IntVar()
+        Checkbutton(self.newWindow, text="(2020/2021", variable=option1, onvalue=1, offvalue=0, **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=2, column=0, sticky=W, padx=5, pady=3)
+        Checkbutton(self.newWindow, text="2022", variable=option2, onvalue=1, offvalue=0, **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=2, column=1, sticky=W, padx=5, pady=3)
+
+        tk.Label(self.newWindow, text="Globalstrahlungsfaktor", **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=3, columnspan=1, sticky=W, padx=5, pady=3)
+        option3 = IntVar()
+        option4 = IntVar()
+        Checkbutton(self.newWindow, text="Ja", variable=option3, onvalue=1, offvalue=0, **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=4, column=0, sticky=W, padx=5, pady=3)
+        Checkbutton(self.newWindow, text="Nein", variable=option4, onvalue=1, offvalue=0, **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=4, column=1, sticky=W, padx=5, pady=3)
+
+        tk.Label(self.newWindow, text="Repowering", **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=5, columnspan=1, sticky=W, padx=5, pady=3)
+        option5 = IntVar()
+        option6 = IntVar()
+        Checkbutton(self.newWindow, text="Ja", variable=option5, onvalue=1, offvalue=0, **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=6, column=0, sticky=W, padx=5, pady=3)
+        Checkbutton(self.newWindow, text="Nein", variable=option6, onvalue=1, offvalue=0, **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=6, column=1, sticky=W, padx=5, pady=3)
+
+        tk.Label(self.newWindow, text="Verbaruchsszenarien", **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=7, columnspan=1, sticky=W, padx=5, pady=3)
+        option7 = IntVar()
+        option8 = IntVar()
+        option9 = IntVar()
+        Checkbutton(self.newWindow, text="Bundesregierung", variable=option7, onvalue=1, offvalue=0, **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=8, column=0, sticky=W, padx=5, pady=3)
+        Checkbutton(self.newWindow, text="alles bleibt gleich", variable=option8, onvalue=1, offvalue=0, **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=8, column=1, sticky=W, padx=5, pady=3)
+        Checkbutton(self.newWindow, text="Sektorenkopplung", variable=option9, onvalue=1, offvalue=0, **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=9, column=0, sticky=W, padx=5, pady=3)
+
+        tk.Label(self.newWindow, text="PR-Faktor verändern?", **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=10, columnspan=1, sticky=W, padx=5, pady=3)
+        option10 = IntVar()
+        option11 = IntVar()
+        Checkbutton(self.newWindow, text="Ja", variable=option10, onvalue=1, offvalue=0, **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=11, column=0, sticky=W, padx=5, pady=3)
+        Checkbutton(self.newWindow, text="Nein", variable=option11, onvalue=1, offvalue=0, **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=11, column=1, sticky=W, padx=5, pady=3)
+
+        tk.Label(self.newWindow, text="Startwert  der Speicher in % : ", **style.STYLE, activebackground=style.BACKGROUND,
+                            activeforeground=style.TEXT).grid(row=12, columnspan=1, sticky=W, padx=5, pady=3)
+
+        ladung = IntVar(self.newWindow)
+        ladung.set(0)
+        self.ladung_spinbox = tk.Spinbox(self.newWindow, width=5, from_=0, to=100, textvariable=ladung)
+        self.ladung_spinbox.grid(row=13, sticky=W, padx=5, pady=3)
+
+        weiter_button = tk.Button(self.newWindow, text='Weiter', **style.STYLE,
+                               activebackground=style.BACKGROUND, activeforeground=style.TEXT,
+                               command=lambda: self.berechnen())
+        weiter_button.grid(row=14, column=0, sticky=E, padx=5, pady=3)
+
+        abbrechen_button = tk.Button(self.newWindow, text='Abbrechen', **style.STYLE,
+                               activebackground=style.BACKGROUND, activeforeground=style.TEXT,
+                               command=lambda: self.newWindow.destroy())
+        abbrechen_button.grid(row=14, column=1, sticky=E, padx=5, pady=3)
+
+        ########################
 
     def add_produktFrame(self):
 
