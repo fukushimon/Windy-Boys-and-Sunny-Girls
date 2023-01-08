46d83d9648000fb647f864a22fd9c8fc310e22bd
fukushimon <shimon-kun@gmx.de>
2023-01-08 11:58:29 +0100

    simulation

--- Anlagen.py
+++ Anlagen.py
@@ -136,7 +136,8 @@ class Brennstoffzelle():
 class Gasnetz():
     def __init__(self, num_elektrolyseure, num_brennstoffzellen, start_charge):
         self.capacity = 0 # MWh
-        self.current_charge = self.capacity * start_charge
+        # self.current_charge = self.capacity * start_charge
+        self.current_charge = 1000000
         self.elec = Elektrolyseur(num_elektrolyseure)
         self.bsz = Brennstoffzelle(num_brennstoffzellen)
         self.gud = GuD()
